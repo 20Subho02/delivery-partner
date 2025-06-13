ab315fa1f14cfe8c5462c4e5b86ddff3c22f6646
Subhajit Chakraborty <csubhajit166@gmail.com>
2025-06-14 01:22:32 +0530

    Update code

--- Delivery-server/server.js
+++ Delivery-server/server.js
@@ -35,10 +35,12 @@ app.use("/images",express.static("uploads"))
 app.use("/api/account",addAccountRouter)
 
 
-app.get("/", (req,res) => {
-    res.send("API Working...")
+// app.get("/", (req,res) => {
+//     res.send("API Working...")
+// });
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
-
-
 
 app.listen(port, () => console.log(`Server started on http://localhost:${port}` .bgCyan.white))
