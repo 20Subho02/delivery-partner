bd8baa7b690ff19cf506f6b8bc590b5a67876d91
Subhajit Chakraborty <csubhajit166@gmail.com>
2025-06-13 12:52:07 +0530

    add folder repo

--- Delivery-server/server.js
+++ Delivery-server/server.js
@@ -14,6 +14,8 @@ const buildpath = path.join(_dirname,"../client/build")
 
 
 
+
+
 //app config
 const app = express()
 const port = process.env.PORT || 5000;
@@ -33,8 +35,12 @@ app.use("/images",express.static("uploads"))
 app.use("/api/account",addAccountRouter)
 
 
-app.get("/", (req,res) => {
-    res.send("API Working...")
+// app.get("/", (req,res) => {
+//     res.send("API Working...")
+// });
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
 app.listen(port, () => console.log(`Server started on http://localhost:${port}` .bgCyan.white))
