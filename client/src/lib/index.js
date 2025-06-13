e403996b79ec5b4cc325ec909bd3afa088d35f29
Subhajit Chakraborty <csubhajit166@gmail.com>
2025-06-13 12:41:28 +0530

    add folder repo

---
+++ client/src/lib/index.js
@@ -0,0 +1,8 @@
+const dev = "http://localhost:5000";
+const prod = "http://http://13.204.88.249:5000";
+
+export const baseURL =
+  window.location.hostname.split(":")[0] === "localhost" ||
+  window.location.hostname.includes("192")
+    ? dev
+    : prod;
